68229a0c6e1fb033ae3421cf05181da3e8da63c6
Sp <Shivpratap715@gmail.com>
2023-06-16 00:33:00 +0530

    change

--- Softomation/TollDataManagement/WebApplication/ATMS-BackOffice/src/app/app.module.ts
+++ Softomation/TollDataManagement/WebApplication/ATMS-BackOffice/src/app/app.module.ts
@@ -5,7 +5,35 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DefaultLayoutComponent } from './_layout/default-layout/default-layout.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatRadioModule} from '@angular/material/radio';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule} from 'primeng/inputnumber';
+import { RadioButtonModule} from 'primeng/radiobutton';
+import { FileUploadModule} from 'primeng/fileupload';
+import { ToastModule} from 'primeng/toast';
+import { ConfirmDialogModule} from 'primeng/confirmdialog';
+import { CalendarModule } from 'primeng/calendar';
+import { OverlayPanelModule} from 'primeng/overlaypanel';
+import { SplitterModule } from 'primeng/splitter';
+import { FieldsetModule } from 'primeng/fieldset';
+import { MessageModule} from 'primeng/message';
+import { PanelModule} from 'primeng/panel';
+import { TableModule } from 'primeng/table';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,8 +44,41 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // MatDialogModule,
-    // NgxSpinnerModule,
+    MatDialogModule,
+    NgxSpinnerModule,
+    ReactiveFormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatInputModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatRadioModule,
+    OverlayModule,
+    DropdownModule,
+    InputNumberModule,
+    RadioButtonModule,
+    FileUploadModule,
+    ConfirmDialogModule,
+    CalendarModule,
+    OverlayPanelModule,
+    ToastModule,
+    SplitterModule,
+    FieldsetModule,
+    MessageModule,
+    PanelModule,
+    TableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
