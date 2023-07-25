c1b768cb92d65c8fec91acb63a0c77ec472dec8b
ANIL KUMAR <77539780+SAGARELECTRONICSINDIA@users.noreply.github.com>
2023-07-25 17:46:42 +0530

    EQM Config

--- Softomation/TollDataManagement/WebApplication/ATMS-BackOffice/src/app/pages/VIDS/vids-equipment-config/vids-equipment-config.component.ts
+++ Softomation/TollDataManagement/WebApplication/ATMS-BackOffice/src/app/pages/VIDS/vids-equipment-config/vids-equipment-config.component.ts
@@ -3,7 +3,7 @@ import { TreeDragDropService } from 'primeng/api';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { apiIntegrationService } from 'src/app/services/apiIntegration.service';
 import { DataModel } from 'src/app/services/data-model.model';
-import { takeUntil } from 'rxjs';
+
 @Component({
   selector: 'app-vids-equipment-config',
   templateUrl: './vids-equipment-config.component.html',
@@ -12,8 +12,9 @@ import { takeUntil } from 'rxjs';
 })
 export class VidsEquipmentConfigComponent implements OnInit {
   availableData: any = [];
-  selection: any;
+  selection: any = null;
   fillData: any = [];
+  FillFinalData: any = [];
   ErrorData: any;
   PermissionData: any;
   DevicesData: any;
@@ -25,6 +26,7 @@ export class VidsEquipmentConfigComponent implements OnInit {
   EquipmentTypeData: any;
   EquipmentDetails: any;
   SystemId = 6;
+  PositionList=[{ID:1,Name:"Entry"},{ID:2,Name:"Exit"},{ID:3,Name:"Main Carriageway"},{ID:4,Name:"Parking Spot"}]
   constructor(private dbService: apiIntegrationService, private dm: DataModel,
     private spinner: NgxSpinnerService) {
     this.LogedUserId = this.dm.getUserId();
@@ -99,18 +101,22 @@ export class VidsEquipmentConfigComponent implements OnInit {
               let onjChild = {
                 key: j + '-' + k,
                 label: element1.EquipmentName + "-(" + element1.IpAddress + "-" + element1.ChainageName + ")",
+                EquipmentTypeId: element1.EquipmentTypeId,
+                ChainageName: element1.ChainageName,
+                EquipmentId: element1.EquipmentId,
+                PositionId:0
               }
               childs.push(onjChild);
             }
             let obj = {
               key: j,
               label: element.EquipmentTypeName,
-              children: childs
+              children: childs,
+              PositionId:0
             }
             this.availableData.push(obj)
           }
         }
-        console.log(this.availableData)
       },
       (error) => {
         this.spinner.hide();
@@ -125,27 +131,108 @@ export class VidsEquipmentConfigComponent implements OnInit {
     );
   }
 
+
+
+  SaveDetails() {
+
+  }
+
+
   onDragStart(val: any) {
-    console.log(val);
+    this.selection = val;
 
   }
 
-  SaveChanges() {
+  onDragEnd() {
+    this.selection = null;
+  }
+  onNodeDrop() {
+    if (this.fillData.length == 0) {
+      if (this.selection.EquipmentTypeId != 28) {
+        this.ErrorData = [{ AlertMessage: 'First Need to add Media Converter !' }];
+        this.dm.openSnackBar(this.ErrorData, false);
+      }
+      else {
+        this.fillData.push(this.selection);
+      }
+    }
+    else {
+      var d1 = this.fillData.filter((e: { EquipmentId: any; }) => e.EquipmentId == this.selection.EquipmentId);
+      if (d1.length > 0) {
+        this.ErrorData = [{ AlertMessage: 'This Equipment already exists !' }];
+        this.dm.openSnackBar(this.ErrorData, false);
+      }
+      else {
+        if (this.selection.EquipmentTypeId != 28) {
+          var d3 = this.fillData.filter((e: { ChainageName: any, EquipmentTypeId: any; }) => e.ChainageName == this.selection.ChainageName && e.EquipmentTypeId == 28);
+          if (d3.length == 0) {
+            this.ErrorData = [{ AlertMessage: 'First Need to add Media Converter !' }];
+            this.dm.openSnackBar(this.ErrorData, false);
+          }
+          else
+            this.fillData.push(this.selection);
+        }
+        else
+          this.fillData.push(this.selection);
+      }
+    }
+    this.ResetNodes();
 
   }
-  onNodeDrop(event) {
-    event.preventDefault();
-        event.stopPropagation();
-    //this.draghoverNode = false;
-}
-onNodeDrop1(event:any){
-    if(event.dragNode.parent==undefined){
-      event.rejected()
-      return false;
-    }
-    else{
-      event.accept()
-      return true;
-    }
+  ResetNodes() {
+    this.FillFinalData=[];
+    var child0 = []
+      var p0 = this.fillData.filter((e: { EquipmentTypeId: any; }) => e.EquipmentTypeId == 28);
+      if (p0.length > 0) {
+        for (let j = 0; j < p0.length; j++) {
+          child0.push(p0[j])
+          let element1=p0[j]
+          
+          var LPUList=[];
+          var p1 = this.fillData.filter((e: { EquipmentTypeId: any,ChainageName:any; }) => e.EquipmentTypeId == 3 && e.ChainageName==element1.ChainageName);
+          if (p1.length > 0) {
+            for (let j = 0; j < p1.length; j++) {
+              LPUList.push(p1[j])
+            }
+          }
+          var CameraList=[];
+          var p2 = this.fillData.filter((e: { EquipmentTypeId: any,ChainageName:any; }) => e.EquipmentTypeId == 25 && e.ChainageName==element1.ChainageName);
+          if (p2.length > 0) {
+            for (let j = 0; j < p2.length; j++) {
+              CameraList.push(p2[j])
+            }
+          }
+          var MDSList=[];
+          var p3 = this.fillData.filter((e: { EquipmentTypeId: any,ChainageName:any; }) => e.EquipmentTypeId == 27 && e.ChainageName==element1.ChainageName);
+          if (p3.length > 0) {
+            for (let j = 0; j < p3.length; j++) {
+              MDSList.push(p3[j])
+            }
+          }
+
+          let mainObj = {
+            key: j,
+            label: element1.label,
+            EquipmentTypeId: element1.EquipmentTypeId,
+            ChainageName: element1.ChainageName,
+            EquipmentId: element1.EquipmentId,
+            LPUList:LPUList,
+            CameraList:CameraList,
+            MDSList:MDSList,
+          }
+          this.FillFinalData.push(mainObj)
+        }
+      }  
+     
+      console.log(this.FillFinalData)
+  }
+
+  childExCol(event) {
+    event.target.parentNode.querySelector(".nested").classList.toggle("nested-active")
+    event.target.classList.toggle("parentNode-down")
+  }
+
+  handleCheck(event:any,row){
+    
   }
 }
