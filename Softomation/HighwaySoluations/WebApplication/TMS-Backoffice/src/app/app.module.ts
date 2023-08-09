47d98d15445e0aa49cda0c26d255b0b58c025583
ANIL KUMAR <77539780+SAGARELECTRONICSINDIA@users.noreply.github.com>
2023-08-09 17:15:21 +0530

    Node Module Updated

--- Softomation/HighwaySoluations/WebApplication/TMS-Backoffice/src/app/app.module.ts
+++ Softomation/HighwaySoluations/WebApplication/TMS-Backoffice/src/app/app.module.ts
@@ -1,8 +1,62 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule, DatePipe } from '@angular/common';
+
+import { HttpClientModule } from '@angular/common/http';
+import { NgApexchartsModule } from 'ng-apexcharts';
+
+import { DropdownModule } from 'primeng/dropdown';
+import { FieldsetModule } from 'primeng/fieldset';
+import { TableModule } from 'primeng/table';
+import { OverlayModule } from 'primeng/overlay';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { CalendarModule } from 'primeng/calendar';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { MessageModule } from 'primeng/message';
+import { PanelModule } from 'primeng/panel';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { TabViewModule } from 'primeng/tabview';
+import { DragDropModule } from 'primeng/dragdrop';
+import { MultiSelectModule } from 'primeng/multiselect';
+
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatRippleModule } from '@angular/material/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { ImageCropperModule } from 'ngx-image-cropper';
+
+export const MY_NATIVE_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD-MMM-YYYY',
+  },
+  display: {
+    dateInput: 'DD-MMM-YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'DD-MMM-YYYY',
+    monthYearA11yLabel: 'MMM YYYY',
+  },
+};
+
+export const MY_CUSTOM_FORMATS = {
+  parseInput: 'l LT',
+  fullPickerInput: 'l LT',
+  datePickerInput: 'l',
+  timePickerInput: 'LT',
+  monthYearLabel: 'MMM YYYY',
+  dateA11yLabel: 'LL',
+  monthYearA11yLabel: 'MMMM YYYY',
+};
 
 @NgModule({
   declarations: [
@@ -10,9 +64,46 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
+    HttpClientModule,
+    NgApexchartsModule,
+
+    DropdownModule,
+    FieldsetModule,
+    TableModule,
+    OverlayModule,
+    InputNumberModule,
+    RadioButtonModule,
+    FileUploadModule,
+    ConfirmDialogModule,
+    CalendarModule,
+    OverlayPanelModule,
+    MessageModule,
+    PanelModule,
+    InputTextModule,
+    PasswordModule,
+    TabViewModule,
+    DragDropModule,
+    MultiSelectModule,
+
+    MatRippleModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatStepperModule,
+    NgxSpinnerModule,
+    ImageCropperModule 
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    [DatePipe],
+    { provide: MAT_DATE_LOCALE, useValue: 'in' }, // you can change useValue
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MY_NATIVE_DATE_FORMATS },
+  ],
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
