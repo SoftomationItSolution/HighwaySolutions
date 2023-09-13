c5476f27cbba98b7e7326a93a77a841c2109cab6
Softomation <hemant.pundir@gmail.com>
2023-09-13 21:09:46 +0530

    up

--- Softomation/HighwaySolutions/Libraries/TMSSystemLibrary/BL/ICDQueryExceptionBL.cs
+++ Softomation/HighwaySolutions/Libraries/TMSSystemLibrary/BL/ICDQueryExceptionBL.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using HighwaySoluations.Softomation.CommonLibrary.IL;
+using HighwaySoluations.Softomation.TMSSystemLibrary.IL;
+using HighwaySoluations.Softomation.TMSSystemLibrary.DL;
 
 namespace HighwaySoluations.Softomation.TMSSystemLibrary.BL
 {
    public class ICDQueryExceptionBL
     {
+        public static List<ResponseIL> Insert(ICDQueryExceptionIL ed)
+        {
+            try
+            {
+                return ICDQueryExceptionDL.Insert(ed);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
     }
 }
